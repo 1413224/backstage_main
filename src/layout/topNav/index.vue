332d45cb08b8c13724d2a0a46e9ad9b4cc35e7ec
Inygl <755508158@qq.com>
2019-10-14 22:34:14 +0800

    修改头部fixed定位

--- src/layout/topNav/index.vue
+++ src/layout/topNav/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="zent-breadcrumb">
     <!-- <span>移动店铺</span> -->
-    <breadCrumb></breadCrumb>
+    <breadCrumb class="breadCrumb fl"></breadCrumb>
     <ul class="navbar-right fr">
       <li class="sfc-solution-user">
         <i class="iconfont icon-unorderedlist"></i>
@@ -64,7 +64,8 @@ export default {
 </script>
 <style lang="less" scoped>
 .zent-breadcrumb{
-  position: relative;
+  position: fixed;
+  width: 100%;
   height: 56px;
   min-width: 750px;
   box-sizing: border-box;
